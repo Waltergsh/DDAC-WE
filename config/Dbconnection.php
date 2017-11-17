5d96dcd92037842675b8d930be3043a3927fb8f5
Waltergsh <33647353+Waltergsh@users.noreply.github.com>
2017-11-17 22:45:46 +0800

    Update Dbconnection.php

--- config/Dbconnection.php
+++ config/Dbconnection.php
@@ -1,8 +1,8 @@
 <?php
 
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
+   define('DB_SERVER', 'uia-ofbs-sea-mysqldbserver.mysql.database.azure.com');
+   define('DB_USERNAME', 'uiaDDAC@uia-ofbs-sea-mysqldbserver');
+   define('DB_PASSWORD', 'u!@DD@c123');
    define('DB_DATABASE', 'uiaflight');
    $connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
